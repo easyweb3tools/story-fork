015f52bd87dbcb29d8d2de14f5ad3e8146105793
bluedao10000 <bluedao10000@gmail.com>
2026-02-13 08:44:07 +0800

    add remotion

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals.js";
 import nextTs from "eslint-config-next/typescript.js";
 
+function asConfigArray(config) {
+  return Array.isArray(config) ? config : [config];
+}
+
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  ...asConfigArray(nextVitals),
+  ...asConfigArray(nextTs),
   globalIgnores([
     ".next/**",
     "out/**",
